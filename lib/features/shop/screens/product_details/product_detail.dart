8a4f491ed90b17923ecaa424b017270d0f69d1b5
DucAnhLe-gif <leducanh02012000@gmail.com>
2024-06-10 17:14:14 +0700

    design detail page more with checkout, description,...

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project4_ngotband_quadat/common/widgets/texts/section_heading.dart';
+import 'package:project4_ngotband_quadat/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
+import 'package:project4_ngotband_quadat/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:project4_ngotband_quadat/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:project4_ngotband_quadat/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:project4_ngotband_quadat/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:project4_ngotband_quadat/utils/constants/sizes.dart';
 import 'package:project4_ngotband_quadat/utils/helpers/helper_functions.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
@@ -11,23 +15,58 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = MyHelperFunctions.isDarkMode(context);
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const MyBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// 1. Product Image Slider
-            MyProductImageSlider(),
+            const MyProductImageSlider(),
 
             /// 2. Product Details
             Padding(
-              padding: EdgeInsets.only(right: MySizes.defaultSpace, left: MySizes.defaultSpace, bottom: MySizes.defaultSpace),
+              padding: const EdgeInsets.only(right: MySizes.defaultSpace, left: MySizes.defaultSpace, bottom: MySizes.defaultSpace),
               child: Column(
                 children: [
                   /// Rating & Share button
-                  MyRatingAndShare(),
+                  const MyRatingAndShare(),
 
                   /// Price, Title, Stock & Brand
-                  MyProductMetaData(),
+                  const MyProductMetaData(),
+
+                  /// Attributes
+                  const MyProductAttributes(),
+                  const SizedBox(height: MySizes.spaceBtwSections,),
+                  /// Checkout Button
+                  SizedBox(width: double.infinity, child: ElevatedButton(onPressed: (){}, child: const Text('Checkout'))),
+                  /// Description
+                  const MySectionHeading(title: 'Description'),
+                  const SizedBox(height: MySizes.spaceBtwItems,),
+                  const ReadMoreText('This is long description This is long description '
+                      'This is long description This is long description'
+                      'This is long description This is long description'
+                      'This is long description This is long description'
+                      'This is long description This is long description'
+                      'This is long description This is long description',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: ' Show more',
+                    trimExpandedText: ' Less',
+                    moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
+                  /// Reviews
+                  const Divider(),
+                  const SizedBox(height: MySizes.spaceBtwItems,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const MySectionHeading(title: 'Reviews (199)', showActionButton: false,),
+                      IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_forward_ios_outlined, size: 18,)),
+                    ],
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwSections,),
                 ],
               ),
             ),
---
+++ lib/features/shop/screens/product_details/widgets/product_attributes.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:project4_ngotband_quadat/common/widgets/chips/choice_chip.dart';
+import 'package:project4_ngotband_quadat/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:project4_ngotband_quadat/common/widgets/texts/product_price_text.dart';
+import 'package:project4_ngotband_quadat/common/widgets/texts/product_title_text.dart';
+import 'package:project4_ngotband_quadat/common/widgets/texts/section_heading.dart';
+import 'package:project4_ngotband_quadat/utils/constants/colors.dart';
+import 'package:project4_ngotband_quadat/utils/constants/sizes.dart';
+import 'package:project4_ngotband_quadat/utils/helpers/helper_functions.dart';
+
+class MyProductAttributes extends StatelessWidget {
+  const MyProductAttributes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MyHelperFunctions.isDarkMode(context);
+    return Column(
+      children: [
+        /// Selected Attribute Pricing & Description
+        MyRoundedContainer(
+          padding: const EdgeInsets.all(MySizes.md),
+          backgroundColor: dark ? MyColors.darkerGrey : MyColors.grey,
+          child: Column(
+            children: [
+              /// Title, Price and Stock Status
+              Row(
+                children: [
+                  const MySectionHeading(title: 'Variation', showActionButton: false,),
+                  const SizedBox(width: MySizes.spaceBtwItems,),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Row(
+                        children: [
+                          MyProductTitleText(title: 'Price', smallSize: true,),
+
+                          /// Actual Price
+                          MyProductPriceText(price: '1000', lineThrough: true,),
+                          SizedBox(width: MySizes.spaceBtwItems,),
+                          /// Sale Price
+                          MyProductPriceText(price: '900', isLarge: true,),
+                        ],
+                      ),
+                      /// Stock
+                      Row(
+                        children: [
+                          const MyProductTitleText(title: 'Stock : ', smallSize: true,),
+                          Text('In Stock', style: Theme.of(context).textTheme.titleMedium,),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+              /// Variation Description
+              const MyProductTitleText(title: 'This is the Description of the Product and it can go up to max 4 lines.',
+              smallSize: true,
+              maxLines: 4,),
+            ],
+          ),
+        ),
+        const SizedBox(width: MySizes.spaceBtwItems,),
+
+        /// Attributes
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const MySectionHeading(title: 'Color', showActionButton: false,),
+            const SizedBox(height: MySizes.spaceBtwItems / 2,),
+            Wrap(
+              spacing: 8,
+              children: [
+                MyChoiceChip(text: 'Green', selected: true, onSelected: (value){},),
+                MyChoiceChip(text: 'Blue', selected: false, onSelected: (value){},),
+                MyChoiceChip(text: 'Yellow', selected: false, onSelected: (value){},),
+              ],
+            )
+          ],
+        ),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const MySectionHeading(title: 'Size'),
+            const SizedBox(height: MySizes.spaceBtwItems / 2,),
+            Wrap(
+              spacing: 8,
+              children: [
+                MyChoiceChip(text: 'S', selected: true, onSelected: (value){},),
+                MyChoiceChip(text: 'M', selected: false, onSelected: (value){},),
+                MyChoiceChip(text: 'L', selected: false, onSelected: (value){},),
+              ],
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
